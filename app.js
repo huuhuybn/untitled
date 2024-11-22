908cc5dd64e4c8b6fa84a19b57f3aa78e2394e7b
HuyNguyen <huuhuybn@gmail.com>
2024-11-22 08:48:42 +0700

    test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var i18n = require('i18n'); // Thêm i18n
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
